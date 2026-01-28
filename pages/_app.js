fb7dd6d5dec3bd399dba84c8e0ecf718743256a8
𝕚𝕞_𝕒𝕓𝕚🌙 <im.abi.cma@gmail.com>
2026-01-28 23:40:22 +0330

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Hina Chess | شطرنج آنلاین</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
+        <meta name="description" content="بازی شطرنج آنلاین دو نفره سریع و مدرن" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
