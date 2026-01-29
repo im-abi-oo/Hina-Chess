ef8a37af0a36cfd60e17df440edba295beca5406
𝕚𝕞_𝕒𝕓𝕚🌙 <im.abi.cma@gmail.com>
2026-01-29 23:01:26 +0330

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,11 @@
+/** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')({
-  dest: 'public',           // محل ذخیره فایل‌های Service Worker و سایر فایل‌های PWA
-  register: true,           // به صورت خودکار Service Worker ثبت می‌شود
-  skipWaiting: true,        // بعد از آپدیت Service Worker جدید، بلافاصله جایگزین می‌شود
-  disable: process.env.NODE_ENV === 'production', // در حالت توسعه، PWA غیرفعال است
-  // runtimeCaching: []     // اگر بخوای می‌تونی کش سفارشی برای فایل‌ها تعریف کنی
-});
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development'
+})
 
 module.exports = withPWA({
-  reactStrictMode: true,    // بررسی سخت‌گیرانه React برای پیدا کردن باگ‌ها
-  swcMinify: true,          // استفاده از SWC برای کاهش حجم کد
-  images: {
-    domains: [],            // اگر تصاویر از دامنه‌های خارجی استفاده می‌کنید، اینجا اضافه کنید
-  },
-  // سایر تنظیمات Next.js مثل redirects یا rewrites هم اینجا اضافه می‌شوند
-});
+  reactStrictMode: false, // برای جلوگیری از رندرهای تکراری در سوکت
+})
