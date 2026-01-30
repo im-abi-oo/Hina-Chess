6b4cbe9f72312d9306dd453d4454692c96aa938c
𝕚𝕞_𝕒𝕓𝕚🌙 <im.abi.cma@gmail.com>
2026-01-30 16:33:11 +0330

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -3,8 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="fa" dir="rtl">
-      <Head />
-      <body>
+      <Head>
+        {/* فونت فارسی پیشنهادی (اگر در پروژه داری لینک کن، وگرنه از فونت سیستم استفاده می‌کند) */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/rastikerdar/vazirmatn@v33.003/Vazirmatn-font-face.css" />
+      </Head>
+      <body className="antialiased dark">
         <Main />
         <NextScript />
       </body>
